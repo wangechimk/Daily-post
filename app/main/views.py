afdc9a0a3d2b8b73b7bf4559217af118a0b0adaa
wangechimk <wangechik.michelle@gmail.com>
2021-04-17 22:03:55 +0300

    add dynamic roots

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,6 @@ from flask import render_template
 def index():
     return render_template("index.html")
 
-@app.route('/sources')
-def sources():
+@app.route('/sources/<name>')
+def sources(name):
     return render_template("sources.html")
