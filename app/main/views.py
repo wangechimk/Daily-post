e3ef30059c010698b2284d14c328fbb49a342ed9
wangechimk <wangechik.michelle@gmail.com>
2021-04-17 22:48:00 +0300

    add function to get all news sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
 from app import app
 from flask import render_template
+from .requests import sources
+import datetime as dt
 
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    news_sources= sources()
+    return render_template("index.html , news_sources=news_sources")
 
 @app.route('/articles/<name>')
 def articles(name):
--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,20 @@
 from app import app
+from .models import Sources
+from newsapi import NewsApiClient
 
-api_key = app.config['API_KEY']
+key = app.config['API_KEY']
+newsapi = NewsApiClient(api_key=key)
+
+
+def sources():
+    '''
+    function that gets all english news sources in a list
+    '''
+    data = newsapi.get_sources(language='en',country='us')
+    data_list = data['sources']
+    source_list=[]
+    for item in data_list:
+        new_source = Sources(item['name'], item['description'], item['url'])
+        source_list.append(new_source)
+
+    return source_list
