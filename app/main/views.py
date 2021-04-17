fec156065036d8ce14d5bb69e369cda95b716901
wangechimk <wangechik.michelle@gmail.com>
2021-04-17 21:59:36 +0300

    create templates

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return "DailyPost2 Homepage"
+    return render_template("index.html")
 
 @app.route('/sources')
 def sources():
-    return "Source articles"
+    return render_template("sources.html")
