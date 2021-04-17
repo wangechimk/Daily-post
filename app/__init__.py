eaedb3f37d4383874de884b78e45f5606ad35d4c
wangechimk <wangechik.michelle@gmail.com>
2021-04-17 22:15:14 +0300

    add configuration classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from config import DevelopmentConfig
 
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
+app.config.from_object(DevelopmentConfig)
+app.config.from_pyfile('config.py')
 
 from app import views 
---
+++ config.py
@@ -0,0 +1,16 @@
+class Config:
+    """
+    """
+    pass
+
+
+class ProdConfig(Config):
+    """
+    """
+    pass
+
+
+class DevConfig(Config):
+    """
+    """
+    DEBUG = True 
