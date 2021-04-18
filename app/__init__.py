8277fbe2c4c24206affaf05c688ab65b251d3f32
wangechimk <wangechik.michelle@gmail.com>
2021-04-18 14:51:23 +0300

    creating a blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
-from config import DevelopmentConfig
+from config import config_options
 
-app = Flask(__name__,instance_relative_config=True)
-app.config.from_object(DevelopmentConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+    
+    app = Flask(__name__)
 
-from app import views 
+app.config.from_object(config_options[config_name])
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .requests import configure_request
+    configure_request(app)
+
+    return app 
